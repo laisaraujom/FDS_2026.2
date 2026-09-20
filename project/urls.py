fdb5b16d31c8315465f05537820e3922c68ad6b3
Laís Moura <mouralais3@gmail.com>
2026-09-20 20:35:33 -0300

    parte 3

--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
